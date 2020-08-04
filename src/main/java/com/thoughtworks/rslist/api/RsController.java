8222d687903b9e3f25c7487c42546c7ad8eb26bc
yi-dai <851758415@qq.com>
2020-08-04 19:57:03 +0800

    test for add one simple RsEvent.

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,18 +1,17 @@
 package com.thoughtworks.rslist.api;
 
 import org.springframework.beans.factory.annotation.Required;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<String> rsList = Stream.of("第一条事件", "第二条事件", "第三条事件").collect(Collectors.toList());
 
   @GetMapping("/rs/list")
   public String getRsList(@RequestParam(required = false) Integer start,
@@ -28,6 +27,10 @@ public class RsController {
     return rsList.get(index - 1).toString();
   }
 
+  @PostMapping("/rs/event")
+  public void addOneRsEvent(@RequestBody String rsEvent){
+    rsList.add(rsEvent);
+  }
 
 
 
--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,6 +36,15 @@ public class RsControllerTest {
     void shouldGetRSEventBetween() throws Exception {
         mockMvc.perform(get("/rs/list?start=1&end=2"))
                 .andExpect(content().string("[第一条事件, 第二条事件]"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldAddOneRsEvent() throws Exception {
+        mockMvc.perform(post("/rs/event").content("第四条事件"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件, 第四条事件]"))
                 .andExpect(status().isOk());
     }
 
