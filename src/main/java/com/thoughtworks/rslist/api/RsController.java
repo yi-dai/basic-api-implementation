7e12904b5859f267b23c006e9b507561740d87eb
yi-dai <851758415@qq.com>
2020-08-05 10:25:19 +0800

    merge data-binding.

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -6,7 +6,6 @@ import com.thoughtworks.rslist.domain.RsEvent;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
